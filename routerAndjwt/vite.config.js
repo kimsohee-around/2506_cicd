deffd72041679c27f28a88771138fe2993776bde
kimsohee <koreait.sec2020@gmail.com>
2025-09-11 01:59:00 +0000

    second

--- routerAndjwt/vite.config.js
+++ routerAndjwt/vite.config.js
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    historyApiFallback: true, // Vite는 기본적으로 지원하지만, 필요 시 명시
-  }
 })
